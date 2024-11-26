c96ff20cda60d17d9f572b6909e829b073337156
Fabian Braun <fsbraun@gmx.de>
2024-11-26 17:34:46 +0100

    Add tests

--- tests/test_html.py
+++ tests/test_html.py
@@ -1,12 +1,16 @@
+from unittest.mock import patch, MagicMock
+
+from cms.api import create_page, add_plugin
 from cms.test_utils.testcases import CMSTestCase
+from django.test import TestCase
+from lxml.etree import Element
 
 from djangocms_text import html, settings
-from djangocms_text.html import render_dynamic_attributes
+from djangocms_text.html import dynamic_href, dynamic_src, render_dynamic_attributes
 from tests.fixtures import TestFixture
 
 
 class HtmlSanitizerAdditionalProtocolsTests:
-
     def test_default_tag_escaping(self):
         settings.TEXT_ADDITIONAL_TAGS = []
         text = html.clean_html(
@@ -19,7 +23,7 @@ class HtmlSanitizerAdditionalProtocolsTests:
         )
 
     def test_custom_tag_enabled(self):
-        settings.TEXT_ADDITIONAL_TAGS = ['iframe']
+        settings.TEXT_ADDITIONAL_TAGS = ["iframe"]
         text = html.clean_html(
             '<iframe src="rtmp://testurl.com/"></iframe>',
             full=False,
@@ -36,12 +40,12 @@ class HtmlSanitizerAdditionalProtocolsTests:
             full=False,
         )
         self.assertEqual(
-            '<span>foo</span>',
+            "<span>foo</span>",
             text,
         )
 
     def test_custom_attribute_enabled(self):
-        settings.TEXT_ADDITIONAL_ATTRIBUTES = ['test-attr']
+        settings.TEXT_ADDITIONAL_ATTRIBUTES = ["test-attr"]
         text = html.clean_html(
             '<span test-attr="2">foo</span>',
             full=False,
@@ -57,10 +61,10 @@ class HtmlSanitizerAdditionalProtocolsTests:
             '<source src="rtmp://testurl.com/">',
             full=False,
         )
-        self.assertEqual('<source>', text)
+        self.assertEqual("<source>", text)
 
     def test_custom_protocol_enabled(self):
-        settings.TEXT_ADDITIONAL_PROTOCOLS = ['rtmp']
+        settings.TEXT_ADDITIONAL_PROTOCOLS = ["rtmp"]
         text = html.clean_html(
             '<source src="rtmp://testurl.com/">',
             full=False,
@@ -77,7 +81,7 @@ class HtmlSanitizerAdditionalProtocolsTests:
             full=False,
         )
         try:
-            self.assertHTMLEqual('<span>foo</span>', cleaned)
+            self.assertHTMLEqual("<span>foo</span>", cleaned)
         finally:
             settings.TEXT_HTML_SANITIZE = old_text_html_sanitize
 
@@ -126,3 +130,63 @@ class HTMLDynamicAttriutesTest(TestFixture, CMSTestCase):
             result,
             '<span data-cms-error="ref-not-found">Link</span>',
         )
+
+
+class DynamicAttributesTestCase(TestCase):
+    @patch("djangocms_text.html.apps.get_model")
+    def test_dynamic_href_sets_correct_attribute(self, mock_get_model):
+        mock_obj = MagicMock()
+        mock_obj.get_absolute_url.return_value = "/test-url"
+        mock_get_model.return_value.objects.filter.return_value = [mock_obj]
+        elem = Element("a", {"data-cms-href": "app.Model:1"})
+        dynamic_href(elem, mock_obj, "href")
+        self.assertEqual(elem.attrib["href"], "/test-url")
+
+    @patch("djangocms_text.html.apps.get_model")
+    def test_dynamic_src_sets_correct_attribute(self, mock_get_model):
+        mock_obj = MagicMock()
+        mock_obj.get_absolute_url.return_value = "/test-url"
+        mock_get_model.return_value.objects.filter.return_value = [mock_obj]
+        elem = Element("img", {"data-cms-src": "app.Model:1"})
+        dynamic_src(elem, mock_obj, "src")
+        self.assertEqual(elem.attrib["src"], "/test-url")
+
+    def test_render_dynamic_attributes_changes_html(self):
+        page = create_page("page", "page.html", language="en")
+        html = f'<a data-cms-href="cms.page:{page.pk}">Link</a>'
+        updated_html = render_dynamic_attributes(html)
+        self.assertIn('<a href="/en/page/">Link</a>', updated_html)
+
+    def test_render_dynamic_attributes_fails(self):
+        html = '<a data-cms-href="app.model:1">Link</a>'
+        updated_html = render_dynamic_attributes(html)
+        self.assertIn('<span data-cms-error="ref-not-found">Link</span>', updated_html)
+
+    def test_render_dynamic_attributes_handles_no_dynamic_attributes(self):
+        html = "<p>No dynamic attributes</p>"
+        updated_html = render_dynamic_attributes(html)
+        self.assertEqual(html, updated_html)
+
+
+def save_image(filename, image, parent_plugin, width, height):
+    pass
+
+
+class DjangoCMSPictureIntegrationTestCase(CMSTestCase):
+    def setUp(self):
+        super().setUp()
+        self.home = self.create_homepage("home", "page.html", "en")
+        self.placeholder = (
+            self.home.pagecontent_set(manager="admin_manager").first().get_placeholders().get(slot="content")
+        )
+
+    def test_extract_images(self):
+        with patch("tests.test_html.save_image") as mock_save_image:
+            add_plugin(
+                self.placeholder,
+                "TextPlugin",
+                "en",
+                body='<img src="https://imageworld.org/"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42m'
+                'P8z/C/HgAGgwJ/lK3Q6wAAAABJRU5ErkJggg==">',
+            )
+            mock_save_image.assert_called_once()

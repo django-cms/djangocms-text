de9210b2f413028c329b88c999005fae4842b0d8
Fabian Braun <fsbraun@gmx.de>
2025-05-12 11:22:31 +0200

    pin to mysql

--- djangocms_text/migrations/0004_remove_old_ckeditor_table.py
+++ djangocms_text/migrations/0004_remove_old_ckeditor_table.py
@@ -6,13 +6,17 @@ def drop_table_if_exists(apps, schema_editor):
     if table_name in schema_editor.connection.introspection.table_names():
         if schema_editor.connection.vendor == "postgresql":
             schema_editor.execute("DROP TABLE IF EXISTS djangocms_text_ckeditor_text CASCADE;")
-        else:
+        elif schema_editor.connection.vendor == "mysql":
+            # MySQL does not support DROP TABLE inside an atomic block
             in_atomic_block = schema_editor.connection.in_atomic_block
             schema_editor.connection.in_atomic_block = False
             try:
                 schema_editor.execute("DROP TABLE IF EXISTS djangocms_text_ckeditor_text;")
             finally:
                 schema_editor.connection.in_atomic_block = in_atomic_block
+        else:
+            # For other databases, we can use the default behavior
+            schema_editor.execute("DROP TABLE IF EXISTS djangocms_text_ckeditor_text;")
 
 
 class Migration(migrations.Migration):

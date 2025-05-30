96879c3dcae300b6fa78240d97fe4222221622cd
Fabian Braun <fsbraun@gmx.de>
2025-05-30 08:48:04 +0200

    Update __init__.py

--- djangocms_text/__init__.py
+++ djangocms_text/__init__.py
@@ -5,10 +5,10 @@ Release logic:
  1. Increase version number (change __version__ below).
  2. Assure that all changes have been documented in CHANGELOG.rst.
  3. In ``pyproject.toml`` check that
-   - versions from all third party packages are pinned in ``REQUIREMENTS``.
-   - the list of ``CLASSIFIERS`` is up to date.
- 4. git add djangocms_text_ckeditor/__init__.py CHANGELOG.rst setup.py
- 5. git commit -m 'Bump to {new version}'
+   - versions from all third party packages are pinned in ``requirements``.
+   - the list of ``classifiers`` is up to date.
+ 4. git add djangocms_text/__init__.py CHANGELOG.rst setup.py
+ 5. git commit -m 'chore: Bump to {new version}'
  6. git push
  7. Assure that all tests pass on https://github.com/django-cms/djangocms-text-ckeditor/actions
  8. Create a new release on https://github.com/django-cms/djangocms-text-ckeditor/releases/new

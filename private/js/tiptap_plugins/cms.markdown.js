74c99538915e0728a314586155cdfeb023a04ac3
Fabian Braun <fsbraun@gmx.de>
2025-06-09 20:19:15 +0200

    fix: also catch md links with javascript: protocol

--- private/js/tiptap_plugins/cms.markdown.js
+++ private/js/tiptap_plugins/cms.markdown.js
@@ -24,7 +24,7 @@ const markdownAntiPatterns = [
     /on(begin|end|repeat|abort|error|resize|scroll|unload|copy|cut|paste|cancel|canplay|canplaythrough|change|click|close|cuechange|dblclick|drag|dragend|dragenter|dragleave|dragover|dragstart|drop|durationchange|emptied|ended|focus|input|invalid|keydown|keypress|keyup|load|loadeddata|loadedmetadata|loadstart|mousedown|mouseenter|mouseleave|mousemove|mouseout|mouseover|mouseup|mousewheel|pause|play|playing|progress|ratechange|reset|resize|scroll|seeked|seeking|select|show|stalled|submit|suspend|timeupdate|toggle|volumechange|waiting|activate|focusin|focusout)\s*=/i,
     /base64,/i,
     /<\s*script/i,
-    /['"]\s*javascript\s*:/i,
+    /['"(]\s*javascript\s*:/i,
 ];
 
 function isProbablyMarkdown(text) {
